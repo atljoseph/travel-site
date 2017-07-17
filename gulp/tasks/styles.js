1b11adb3ecd5ba4055f1e0d0cfbc037c08ed77c5
Joseph <joseph.gill.atlanta@gmail.com>
2017-07-17 16:47:26 -0700

    Completed large hero block and intro block content.

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+cssnested = require('postcss-nested'),
+cssimport = require('postcss-import'),
+cssmixins = require('postcss-mixins');
+
+// css processing
+gulp.task('styles', function() {
+    // console.log('imagine SASS or PostCSS tasks here!');
+    // convert postCSS to CSS
+    // gulp.src is ASYNC - gulp must be aware when this function completes
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssimport, cssmixins, cssvars, cssnested, autoprefixer]))
+        .on('error', function(errorInfo){
+            console.log(errorInfo.toString());
+            // gracefully end the task before gulp finds out about the error
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./app/temp/styles'));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+// gulp watch plugin
+// can also be ran/tested in git bash as 'gulp watch'
+gulp.task('watch', function() {
+    console.log('gulp watch task is running');
+    
+    // tell browsersync how it should initialise
+    browserSync.init({
+        // when gulp watch is ran, this line causes a browser to open to http://local;host:3000
+        notify: false, 
+        server: {
+            baseDir: "app"
+        }
+    });
+    
+    // when index.html changes, start the 'html' task
+    watch('./app/index.html', function(){
+        // gulp.start('html');    
+        browserSync.reload();    
+    });
+
+    // watch src css files for changes
+    watch('./app/assets/styles/**/*.css', function(){
+        // give the processed css to the browser
+        gulp.start('cssInject');        
+    });
+
+
+});
+
+// css injection task
+// 'styles' task is a dependency of 'cssInject' task
+gulp.task('cssInject', ['styles'], function() {
+    // give the css to browserSync
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
